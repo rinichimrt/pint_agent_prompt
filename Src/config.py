af35541880d0b8a390ff043b40d1739f13987453
chri22024 <chri22024@g.nihon-u.ac.jp>
2025-06-11 16:12:50 +0900

    変更

--- Src/config.py
+++ Src/config.py
@@ -21,17 +21,26 @@ DEFAULT_MODEL_NAME = "gemma3:latest"  # 例: "llama3", "mistral", "phi3"
 # DEFAULT_TARGET_ACTOR_ROLE_KEY = "code_explainer"
 # DEFAULT_TARGET_ACTOR_ROLE_KEY = "dialogue_partner_neutral"
 # DEFAULT_TARGET_ACTOR_ROLE_KEY = "pint_agent_2_JA"
-DEFAULT_TARGET_ACTOR_ROLE_KEY = "pint_agent_2_EN"
+# DEFAULT_TARGET_ACTOR_ROLE_KEY = "pint_agent_2_EN"
+DEFAULT_TARGET_ACTOR_ROLE_KEY = "pint_agent_boss_with_feedback"
 
 # デフォルトの対話シナリオのキー
 # DIALOGUE_SCENARIOS_KEY = "SCENARIO_PINT_AGENT_TEST_JA"
-DIALOGUE_SCENARIOS_KEY = "SCENARIO_PINT_AGENT_TEST_EN"
+# DIALOGUE_SCENARIOS_KEY = "SCENARIO_PINT_AGENT_TEST_EN"
+DIALOGUE_SCENARIOS_KEY = "SCENARIO_BOSS_PERSUASION_JA"
 
 
 # --- 再現性設定 ---
 # デフォルトのシード値。Noneにすると、実行ごとに結果が変動しやすくなります。
 # 特定の数値を設定すると、同じ入力に対して（ほぼ）同じ出力が得られるようになります。
 DEFAULT_SEED = 459
+
+
+# 入力を英語にするか日本語にするか
+# ENHANCED_USER_UTTERANCE_KEY= EN
+ENHANCED_USER_UTTERANCE_KEY = JA
+
+
 
 
 # --- (オプション) 対話履歴の保存設定 ---
